36ce79a4ea21f98cc4bfcba072df43aa6cb6356c
Vishula <vishula.gamaetige@my.jcu.edu.au>
2020-08-25 12:47:00 +1000

    More Quetions completed

--- PageOne.py
+++ PageOne.py
@@ -1,3 +1,5 @@
+import fact as fact
+
 vish = 1
 # # Write a Python program to print the following string in a specific format
 # # Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
@@ -81,7 +83,6 @@ vish = 1
 # ---------------------------------------------------------------------------
 
 vish = 2
-
 
 # val = input("(E)nter numbers: ")
 # list = val.split(",")
@@ -325,13 +326,285 @@ vish = 2
 
 # create a histrogram from a given list of integers
 
-def histogram(items):
-    for n in items:
-        output = ''
-        times = n
-        while(times > 0):
-          output += 'x'
-          times = times -1
-        print(output)
+# def histogram(items):
+#     for n in items:
+#         output = ''
+#         times = n
+#         while(times > 0):
+#           output += 'x'
+#           times = times -1
+#         print(output)
+#
+# histogram([2, 3, 6, 5])
 
-histogram([2, 3, 6, 5])
+# ---------------------------------------------------------------------------
+# Q 27]]
+# emptystring
+# list to a string
+
+# print
+# def lststring(lisa):
+#     emptystring = ""
+#     for x in lisa:
+#
+#         convertedinput = str(x)
+#         emptystring += convertedinput
+#     print(emptystring, end=".")  # add "." to end after one loop.
+#
+#
+# lststring(["vish", "got", 1, "Wright"])
+
+# ---------------------------------------------------------------------------
+# Q 28 & 29 & 30 already done. find and add
+
+# ---------------------------------------------------------------------------
+# Q 31 already done. find and add
+
+#
+
+# def gcd(x, y):
+#     xval = int(input("Enter x value: "))
+#     yval = int(input("Enter y value: "))
+#
+#     while xval > 0:
+import math
+# def factos(a):
+# #     if a > 0:
+# #         return a * math.factorial(a-1)
+# #     else:
+# #         return 1
+# #
+# #
+# # print(factos(4))
+# # print(factos(3))
+# # print(factos(2))
+# # print(factos(1))
+# # print(factos(0))
+
+# import numbers
+# import fibonacci
+# def fibon(n, index):
+#     if index[n] != 0:
+#         return index[n]
+#     if n == 2 or n == 1:
+#         result = 1
+#     else:
+#         result = fibonacci(n-1) + fibonacci(n-2)
+#     return result
+#
+# print(fibon(5), 6)
+
+# ---------------------------------------------------------------------------
+# Q 32 find lcm of two (+) integers
+
+# def gcd(x, y):
+#     if y == 0:
+#         return x
+#     else:
+#         return gcd(y,x%y)
+#
+# def lcm(a, b):
+#     lcm = (a * b)/gcd(a, b)
+#     return lcm
+#
+# print(lcm(50, 65))
+
+
+# def lcm(x, y):
+#    if x > y:
+#        z = x
+#    else:
+#        z = y
+#
+#    while(True):
+#        if((z % x == 0) and (z % y == 0)):
+#            lcm = z
+#            break
+#        z += 1
+#
+#    return lcm
+# print(lcm(4, 6))
+# print(lcm(15, 17))
+
+# ---------------------------------------------------------------------------
+# Q 33 program sum of 3 given integers
+# if 2 values are same sum = 0
+
+# def summ(a, b, c):
+#
+#     if a == b or b == c or a ==c:
+#         summ = 0
+#
+#     else:
+#         summ = a + b + c
+#     return summ
+#
+#
+#
+# print(summ(10,20,30))
+# print(summ(20,50,20))
+
+
+# ---------------------------------------------------------------------------
+# Q 34 program sum of 3 given integers
+
+# def twosome(a, b):
+#     sum = a + b
+#     if sum in range(15,20):
+#         return 20
+#     else:
+#         return sum
+#
+# print(twosome(10,6))
+# print(twosome(10,2))
+# print(twosome(10,12))
+
+# ---------------------------------------------------------------------------
+# Q 35
+# two integers are equal or difference = 5
+# or sum is 5
+# def program(a, b):
+#     if a == b or a + b == 5 or a - b == 5:
+#         return True
+#     else:
+#         return False
+#
+# print(program(5,5))
+# print(program(10, 5))
+# print(program(10,0))
+
+# ---------------------------------------------------------------------------
+# Q 36
+
+# def uzi(objectA, objectB):
+#     if isinstance(objectA, int) and isinstance(objectB, int):
+#         return objectA + objectB
+#     else:
+#
+#         return "Error"
+#
+# print(uzi(10,20))
+# print(uzi(1.3,5))
+# print(uzi("a", "b"))
+
+# ---------------------------------------------------------------------------
+# Q 37
+
+
+# name = input("Enter name: ")
+# age = int(input("Enter age: "))
+# adde = input("Enter Address: ")
+#
+# print(name, age, adde)
+
+# ---------------------------------------------------------------------------
+# Q 38
+# (x*y) * (x*y)
+
+# def mult(x, y):
+#     a = x ** 2 + y **2 + 2*x*y
+#     print("({} + {}) ^ 2) = {}".format(x, y, a))
+#
+# mult(4,3)
+
+# ---------------------------------------------------------------------------
+# Q 39
+
+# amt = 10000
+# int = 3.5
+# years = 7
+#
+# future_value  = amt*((1+(0.01*int)) ** years)
+# print(round(future_value,2))
+
+# ---------------------------------------------------------------------------
+# Q 40
+# calculating distance between two points
+# pythagoras theorem used for final equation
+# import math
+# p1 = [4, 0]
+# p2 = [6, 6]
+#
+# # use index to refer to x1 and x2 and y1 and y2
+# distance = math.sqrt((p1[0] - p2[0])**2 + (p1[1]-p2[1])**2)
+# print(distance)
+
+# ---------------------------------------------------------------------------
+# Q 41
+# program to check if a file exist in a current folder
+
+# import os.path
+#
+# print(os.path.isfile('abc.txt'))
+# print(os.path.isfile("PageOne.py"))
+
+# ---------------------------------------------------------------------------
+# Q 42 later
+
+# ---------------------------------------------------------------------------
+# Q 43
+# program to get os name
+# platform and release info
+# import platform
+# import os
+#
+# print(os.name)
+# print(platform.system())
+# print(platform.release())
+
+# ---------------------------------------------------------------------------
+# Q 44
+# locate py site packages
+# import site;
+# print(site.getsitepackages())
+# ---------------------------------------------------------------------------
+# Q 45
+# program to call an external command in py
+
+# from subprocess import call
+# call(['ls', '-l'])
+
+# ---------------------------------------------------------------------------
+# Q 46
+# program to get the path
+# and name of the file currently executing
+
+# import os
+# print("current file name: ", os.path.realpath(__file__))
+
+# ---------------------------------------------------------------------------
+# Q 47
+# program to find out number of CPU's using
+import multiprocessing
+
+print(multiprocessing.cpu_count())
+
+
+# ---------------------------------------------------------------------------
+# Q 48
+# program to parse/send a string to
+# float or integer
+# word = float(input("Enter a string: "))
+# float = float(word)
+# integer = int(word)
+# print("Float: ", float)
+# print("Integer: ", integer)
+
+
+# def string2fi(word):
+#
+#     convertedfloat = float(word)
+#     convertedint = int(float(word))
+#     print(convertedfloat)
+#     print(convertedint)
+#
+# string2fi("246.231")
+
+# ---------------------------------------------------------------------------
+# Q 49
+#
+
+from os import listdir
+from os.path import isfile, join
+files_list = [f for f in listdir('/Users/Vishula/Desktop') if isfile(join('/Users/Vishula/Desktop', f))]
+print(files_list);
